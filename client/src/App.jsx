8de89d4dd2ceb05aa4213153db07277f96ab46ba
Yan Dias <yandiasferreira@gmail.com>
2025-07-28 01:52:06 -0300

    adiciona Route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,23 @@
-// src/App.jsx
-
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
-// 1. Importa os componentes de página da pasta 'pages'
 import LoginPage from './pages/Login';
 import RegisterPage from './pages/Register';
-// No futuro, você pode adicionar outras páginas aqui
-// import DashboardPage from './pages/Dashboard';
+import TabelaUsuariosPage from './pages/TabelaUsuarios';
 
-import './App.css'; // Estilos do formulário
+import './App.css'; // todo o css
 
 function App() {
   return (
-    // 2. 'Routes' é o container para todas as suas rotas
     <Routes>
-      {/* Rota inicial: redireciona para a página de login */}
       <Route path="/" element={<Navigate to="/login" />} />
 
-      {/* Rota para a página de login */}
       <Route path="/login" element={<LoginPage />} />
 
-      {/* Rota para a página de registro */}
       <Route path="/register" element={<RegisterPage />} />
 
-      {/* Exemplo de rota futura para o dashboard (protegida) */}
-      {/* <Route path="/dashboard" element={<DashboardPage />} /> */}
+      <Route path="/tabelaUsuarios" element={<TabelaUsuariosPage/>}/>
+
     </Routes>
   );
 }
